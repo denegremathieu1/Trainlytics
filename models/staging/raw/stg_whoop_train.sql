0d3ae6ab6fcbc07ab2df4c484a3a25ac5456638f
Mathieu Denegre <denegremathieu1@gmail.com>
2026-01-12 15:29:44 +0000

    Mise à jour colonne Duree_minute

--- models/staging/raw/stg_whoop_train.sql
+++ models/staging/raw/stg_whoop_train.sql
@@ -1,6 +1,6 @@
 SELECT
     DATE(Heure_de_fin_de_l_entrainement) AS Date
-    , ROUND(Duree_min / 60,0) AS Duree_heure
+    , Duree_min
     , Nom_de_l_activite
     , Effort_activite
     , Zone_FC_1
